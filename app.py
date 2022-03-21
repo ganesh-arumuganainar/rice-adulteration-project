95f6d9cc91322c0a020a29d1287b0f8bf0c41586
Ganesh Arumuganainar <61970474+ganesh-arumuganainar@users.noreply.github.com>
2022-03-21 11:57:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from flask import Flask, request, redirect, render_template
 from werkzeug.utils import  secure_filename
 import os
@@ -6,13 +7,15 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image, ImageOps
 
-rice_classes = ['Basmati', 'Gundu Malli']
+rice_classes = ['Basmati', 'Gundu Malli', 'Raw Rice']
+cereal_classes = ['Black Gram', 'Cow Peas', 'Green Moong Dal', 'Horse Gram']
 application = Flask(__name__, static_url_path='/Users/ganesh/Downloads/Rice_Quality_Project_0.1/static')
 
 application.config["IMAGE_UPLOADS"] = 'static'
 application.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG","JPG","PNG"]
 
-model = tf.keras.models.load_model('model')
+rice_model = tf.keras.models.load_model('models/rice_model')
+cereal_model = tf.keras.models.load_model('models/cereal_model')
 
 def allowed_image(filename):
 
@@ -32,6 +35,9 @@ def allowed_image(filename):
 def upload_image():
     if request.method=="POST":
 
+        category = request.form.get("subject")
+        subCategory = request.form.get("topic")
+
         if request.files:
 
             image = request.files["image"]
@@ -46,7 +52,7 @@ def upload_image():
                 
                 image.save(os.path.join(application.config["IMAGE_UPLOADS"], filename))
 
-                return redirect(f'/showing-image/{filename}')
+                return redirect(f'/showing-image/{filename}/{category}/{subCategory}')
 
             else:
 
@@ -56,9 +62,9 @@ def upload_image():
 
 
 
-@application.route("/showing-image/<image_name>", methods=["GET", "POST"])
+@application.route("/showing-image/<image_name>/<category>/<subCategory>", methods=["GET", "POST"])
 
-def showing_image(image_name):
+def showing_image(image_name, category, subCategory):
     if request.method == "POST":
         
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
@@ -68,9 +74,34 @@ def showing_image(image_name):
         image_array = np.asarray(image)
         normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
         data[0] = normalized_image_array
-        prediction = model.predict(data)
-        gunduMalli= round(prediction[0][0]*100,2)
-        basmati= round(prediction[0][1]*100,2)
+        
+        if category == "Rice":
+            model = rice_model
+            prediction = model.predict(data)
+            gunduMalli= round(prediction[0][1]*100,2)
+            basmati= round(prediction[0][0]*100,2)
+            rawRice = round(prediction[0][2]*100,2)
+
+            if rice_classes[np.argmax(prediction[0])]== subCategory and np.max(prediction[0]>=0.7):
+                result = 'The variety is suitable for consumption'
+            else:
+                result = 'The variety is too much adulterated and not suitable for consumption'
+            return render_template("prediction_result.html", image_name=image_name, category=category, subCategory=subCategory, gunduMalli=gunduMalli, basmati=basmati, rawRice=rawRice, result=result)
+
+        
+        else:
+            model = cereal_model
+            prediction = model.predict(data)
+            blackGram= round(prediction[0][0]*100,2)
+            cowPeas= round(prediction[0][1]*100,2)
+            greenMoongDal = round(prediction[0][2]*100,2)
+            horseGram = round(prediction[0][3]*100,2)
+            if cereal_classes[np.argmax(prediction[0])]== subCategory and np.max(prediction[0]>=0.7):
+                result = 'The variety is suitable for consumption'
+            else:
+                result = 'The variety is too much adulterated and not suitable for consumption'
+            return render_template("prediction_result2.html", image_name=image_name, category=category, subCategory=subCategory, blackGram=blackGram, cowPeas=cowPeas, greenMoongDal=greenMoongDal, horseGram=horseGram, result=result)
+
         
         """ image = cv2.imread(image_path)
         img = image.copy()
@@ -79,7 +110,6 @@ def showing_image(image_name):
         image = image.astype("float32")
         image = image/255.0
         np_image = np.expand_dims(image, axis=0)
-
         predictions = model(np_image)
         max_class = np.argmax(predictions)
         min_class = np.argmin(predictions)
@@ -88,16 +118,10 @@ def showing_image(image_name):
         predicted_max_class = rice_classes[max_class]
         predicted_min_class = rice_classes[min_class] """
 
-        return render_template("prediction_result.html", image_name=image_name, gunduMalli=gunduMalli, basmati=basmati)
-
+        
     return render_template("showing_image.html", value=image_name)
 
 
 
 if __name__ == '__main__':
     application.run(port=5000)
-
-
-
-
-
